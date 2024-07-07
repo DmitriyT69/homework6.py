71e9fd4449c355b3322dffa3d4d2cd946269c429
DmitriyT69 <td2598793@gmail.com>
2024-07-07 23:00:32 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_dict = {'Kiril': 1995, 'Sasha': 2000}
+print(my_dict)
+print(my_dict['Kiril'])
+my_dict['Vanya'] = 2007
+my_dict['Alex'] = 2005
+del my_dict['Alex']
+print(my_dict)
+my_set = {1, 2, 'String', False, 1, 2, False, 'String'}
+print(my_set)
+my_set.add(5)
+my_set.add(56)
+my_set.remove(2)
+print(my_set)
